2b24065cb62356ee360ba60b31862116a9f2652e
John McDowell <johnmcdowell0801@gmail.com>
2018-07-02 12:20:52 +0000

    pagination implemented. pagination put at the bottom of the base.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Product, ProductImage
+from django.core.paginator import Paginator
+
+def paginate(request, products):
+    paginator = Paginator(products, 5)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+    return paginator, page, products
+    
 
 def all_products(request):
     products = Product.objects.all()
@@ -13,33 +21,38 @@ def product_details(request, pk):
     
 def get_acoustic(request):
     products = Product.objects.filter(type="ACOUSTIC")
+    paginator, page, products = paginate(request, products)
     return render(request, "products/acoustic.html", {'products': products})
     
 def get_electric(request):
     products = Product.objects.filter(type="ELECTRIC")
+    paginator, page, products = paginate(request, products)
     return render(request, "products/electric.html", {'products': products})
     
 def get_bass(request):
     products = Product.objects.filter(type="BASS")
+    paginator, page, products = paginate(request, products)
     return render(request, "products/bass.html", {'products': products})
     
 def get_amps(request):
     products = Product.objects.filter(type="AMPLIFIER")
+    paginator, page, products = paginate(request, products)
     return render(request, "products/amps.html", {'products': products})
     
 def get_effects(request):
     products = Product.objects.filter(type="EFFECTS")
+    paginator, page, products = paginate(request, products)
     return render(request, "products/effects.html", {'products': products})
     
 def get_pickups(request):
     products = Product.objects.filter(type="PICKUP")
+    paginator, page, products = paginate(request, products)
     return render(request, "products/pickups.html", {'products': products})
     
 def get_audio(request):
     products = Product.objects.filter(type="AUDIO")
+    paginator, page, products = paginate(request, products)
     return render(request, "products/audio.html", {'products': products})
-    
-    
     
 def add_to_cart(request):
     product_to_add = get_object_or_404(Product, pk=request.POST["product_id"])
