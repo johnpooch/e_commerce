749c7c4f1b5f00f7697c579c0082202fcb16a86b
John McDowell <johnmcdowell0801@gmail.com>
2018-08-23 14:35:21 +0000

    news post detail looking good. could use a 'more news' section

--- static/js/navbarColor.js
+++ static/js/navbarColor.js
@@ -1,5 +1,5 @@
 // Changes the color of the navbar on the products templates
 
 $(function() {
-    $( "#nav-div" ).css({"background-color": "black", "border-bottom": "solid rgba(255, 0, 0, 0.41) 1px"});
+    $( "#nav-div" ).css({"background-color": "black", "border-bottom": "solid rgb(224, 39, 39) 2px"});
 });
